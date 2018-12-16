c980e547097fcdf8d2ee1c0272759bc523e2bb6f
Maxime AUBLET <maximeaublet@gmail.com>
2018-12-16 20:07:51 +0100

    [WIP] Exercice 2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -2,43 +2,43 @@
 
 // 1 - Vérifie le type de l'objet passé en param
 function type_check_v1(obj, type) {
-    if (typeof (type) !== "string")
-        return null;
-
-    if (type === 'null')
-        return elem === null;
-
-    if(type === 'array')
-        return Array.isArray(obj)
-
-    return (typeof(obj) == type);
+  if (typeof type !== "string") return null;
+  if (type === "null") return obj === null;
+  if (type === "array") return Array.isArray(obj);
+  return typeof obj === type;
 }
 
 // 2 - Vérifie le type de l'objet passé en param
 function type_check_v2(obj, conf) {
-
-    // On regarde l'enumeration
-    if (conf.enum) {
-        bool_enum = false
-        conf.array.forEach(e => {
-            if(e == conf.enum){
-                bool_enum = true
-            }
-        });
-    }
-
-    if(conf.type)
-        bool_type = type_check_v1(obj, conf["type"])
-
-    if(conf.value)
-        bool_value = obj == conf.value
-
-    return (bool_enum || (bool_type && bool_value))
+  if (conf.hasOwnProperty("type")) {
+    if (conf.hasOwnProperty("value"))
+      return conf.value == obj && type_check_v1(obj, conf.type);
+    else return type_check_v1(obj, conf.type);
+  } else if (conf.hasOwnProperty("enum")) return conf.enum.includes(obj);
+  return false;
 }
 
+// 3 - Vérification de type récursif
+function type_check(val, check) {
+  return false;
+}
 
 // ========================================================
 //                      TESTS
 // ========================================================
 
-console.log(type_check_v1(1, "number"))
+var test = {
+  properties: {
+    prop1: { type: "number" },
+    prop2: { type: "string", enum: ["val1", "val2"] },
+    prop3: { type: "object", properties: { prop31: "number" } },
+    prop4: { type: "array", properties: ["boolean"] }
+  }
+};
+
+console.log("--- TYPE_CHECK_V1 ---");
+console.log(type_check_v1(1, "number"));
+console.log("--- TYPE_CHECK_V2 ---");
+console.log(type_check_v2(1, { type: "number", value: 1 }));
+console.log(type_check_v2("foo", { enum: ["foo", "bar", 3] }));
+console.log(type_check("azerty", test));
